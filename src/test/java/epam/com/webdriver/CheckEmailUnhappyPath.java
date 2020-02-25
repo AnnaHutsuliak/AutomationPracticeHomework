01d7fa7ee76487d9ba7af3418ca5812c0080cac6
Anna Hutsuliak <anhutsuliak@gmail.com>
2020-02-25 14:43:23 +0200

    Initial commit

---
+++ src/test/java/epam/com/webdriver/CheckEmailUnhappyPath.java
@@ -0,0 +1,20 @@
+package epam.com.webdriver;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CheckEmailUnhappyPath {
+
+    @DataProvider(name = "InValidEmail")
+    public Object[] invalidEmail() {
+        return new Object[]{"i@1gmail.com", " @gmail.com", "12_anna@ ", "An_1@2.gmail.com",
+                "1a_Agmail.com", "aa@gmail.com", "annA!_Gnna123asqhgfdh@gmail.com", "anna123@g.c",
+                "anna123@GMAILLLLLL.COMMMMMM","№ `?*+`1=3@g ail.com", " ", "0", }; }
+
+    @Test(dataProvider = "InValidEmail")
+    public void checkEmail (String email){
+        boolean actualResult = Email.isEmailCorrect(email);
+        Assert.assertFalse(actualResult, "this email is not correct");
+    }
+}
