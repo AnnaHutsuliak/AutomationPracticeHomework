75a52ba068cae6bec3f2bd272020797f84554fc5
AnnaHutsuliak <59147033+AnnaHutsuliak@users.noreply.github.com>
2020-02-25 11:12:18 +0200

    Update CheckAssertions.java
    добавила message

--- AutomationPractice/src/test/java/epam/com/webdriver/CheckAssertions.java
+++ AutomationPractice/src/test/java/epam/com/webdriver/CheckAssertions.java
@@ -13,11 +13,11 @@ public class CheckAssertions {
             driver.navigate().to("http://google.com");
             Assert.assertEquals("Google", driver.getTitle());
             Assert.assertEquals("Gogle", driver.getTitle());
-            Assert.assertTrue(driver.getTitle().startsWith("Goo"));
-            Assert.assertTrue(driver.getTitle().endsWith("gle"));
-            Assert.assertTrue(driver.getTitle().endsWith("le"), "It is true =)");
-            Assert.assertFalse(driver.getTitle().startsWith("gla"));
-            Assert.assertFalse(driver.getTitle().endsWith("gli"));
-            Assert.assertFalse(driver.getTitle().endsWith("gli"),"It is false =(" );
+            Assert.assertTrue(driver.getTitle().startsWith("Goo"), "Title starts with 'Goo', true");
+            Assert.assertTrue(driver.getTitle().endsWith("gle"), "Title ends with 'gle', true, true");
+            Assert.assertTrue(driver.getTitle().endsWith("le"), "Title ends with 'le', true");
+            Assert.assertFalse(driver.getTitle().startsWith("gla"), "Title starts with 'gla', false");
+            Assert.assertFalse(driver.getTitle().endsWith("gli"), "Title ends with 'gli', false");
+            Assert.assertFalse(driver.getTitle().endsWith("glg"),"Title ends with 'glg', false" );  
         }
 }
