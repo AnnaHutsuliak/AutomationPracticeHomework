0d1ce60bcfc33676da94e7e7cbee97b124ab7469
AnnaHutsuliak <59147033+AnnaHutsuliak@users.noreply.github.com>
2020-02-25 11:14:03 +0200

    Update CheckEmailHappyPath.java
    добавила граничні тести та message.

--- AutomationPractice/src/test/java/epam/com/webdriver/CheckEmailHappyPath.java
+++ AutomationPractice/src/test/java/epam/com/webdriver/CheckEmailHappyPath.java
@@ -8,15 +8,13 @@ public class CheckEmailHappyPath{
 
     @DataProvider(name = "ValidEmail")
     public Object[] validEmail() {
-        return new Object[]{"anna123@gmail.com", "an12nna@gmail.com", "12_anna@gmail.com", "An_1@gmail.com",
-                "1a_A@gmail.com"}; }
+          return new Object[]{"an12nna@1mail.com", "12_anna@GMAIL.com", "An_1@gmail.com",
+                "1a_A@gm.co", "aaQaaa1Af!aaaa_aaAf1@Gmail.commm", "a_!@gmail.com"}; }
 
         @Test(dataProvider = "ValidEmail")
-
         public void checkEmail (String email){
-
             boolean actualResult = Email.isEmailCorrect(email);
-            Assert.assertTrue(actualResult);
+            Assert.assertTrue(actualResult, "this email is correct");
         }
     }
 
